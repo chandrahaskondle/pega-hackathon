4f7351ceaf84e59079e5d526ff9b55d4995d0ce7
pokam <madhukar.pokala@in.pega.com>
2021-11-25 12:11:39 +0530

    initial backend code commit

---
+++ GovtHCMS_backend/src/main/java/com/pega/hackathon/healthcare/GovtHcmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pega.hackathon.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovtHcmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovtHcmsBackendApplication.class, args);
+    }
+
+}
